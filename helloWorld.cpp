3a35361c4d97c0905c85d734362d109316ad7a45
Greco <rgreco.pedro@gmail.com>
2024-12-26 14:27:59 -0300

    Added the two existing files to the repository

---
+++ helloWorld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+//A program that serves the purpose of being a syntax of every basic c++ thing, devoid of logic at first, logic exercises will soon follow
+//will start exploring a combination of function structure
+
+
+int main(){
+
+    cout<< "Hello World"<<endl;
+
+    return 0;
+}
